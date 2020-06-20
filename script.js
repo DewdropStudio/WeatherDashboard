b9a589c3e091235a99c42655e1e754ec49177d15
Rocio Avila <rocio.avila@northwestern.com>
2020-06-19 20:55:52 -0500

    Added OpenWeather url and api key

---
+++ script.js
@@ -0,0 +1,9 @@
+var apiURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=";
+
+var currentURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+APIkey = "&appid=d4f44206d0b22404f046fb4a53b86024";
+
+    queryurl = url + city + APIkey;
+    
+    current_weather_url = currenturl + city + APIkey; 
